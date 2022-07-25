1d83fc291e90860996150a5859c8438dd94248de
Ronald <rjskinnerc@unal.edu.co>
2022-07-25 14:39:40 -0500

    Move run function to main scope

--- app.rb
+++ app.rb
@@ -5,28 +5,11 @@ require './rental'
 
 class App
   def initialize
-    @options = [
-      { id: 1, name: '1 - List all books', action: 'list_books' },
-      { id: 2, name: '2 - List all people', action: 'list_people' },
-      { id: 3, name: '3 - Create a person', action: 'create_person' },
-      { id: 4, name: '4 - Create a book', action: 'create_book' },
-      { id: 5, name: '5 - Create a rental', action: 'create_rental' },
-      { id: 6, name: '6 - List all rentals for a given person id', action: 'list_rentals' },
-      { id: 7, name: '7 - Exit', action: 'exit_app' }
-    ]
-
     @books = []
     @people = []
     @rentals = []
     puts 'Welcome to School Library App!'
     puts ''
-  end
-
-  def run
-    puts 'Please choose an option by entering a number:'
-    @options.each { |option| puts option[:name] }
-    choice = gets.chomp.to_i
-    action choice
   end
 
   def action(choice)
--- main.rb
+++ main.rb
@@ -1,9 +1,25 @@
 #!/usr/bin/env ruby
 require './app'
+def run  
+  @options = [
+    { id: 1, name: '1 - List all books', action: 'list_books' },
+    { id: 2, name: '2 - List all people', action: 'list_people' },
+    { id: 3, name: '3 - Create a person', action: 'create_person' },
+    { id: 4, name: '4 - Create a book', action: 'create_book' },
+    { id: 5, name: '5 - Create a rental', action: 'create_rental' },
+    { id: 6, name: '6 - List all rentals for a given person id', action: 'list_rentals' },
+    { id: 7, name: '7 - Exit', action: 'exit_app' }
+  ]
+
+  puts 'Please choose an option by entering a number:'
+  @options.each { |option| puts option[:name] }
+  choice = gets.chomp.to_i
+  return choice
+end
 
 def main
   app = App.new
-  app.run
+  run
 end
 
 main
