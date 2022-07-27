e027b373f3a6cdfa4498a98f3ba7840242cd9913
Santiago <alexander.cardenas10@outlook.com>
2022-07-27 12:05:26 -0500

    Add method tests to person class

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -12,4 +12,21 @@ describe Person do
     expect(person_object.name).to eq 'Person'
     expect(person_object.parent_permission).to eq true
   end
+
+  it 'should return true if age is higher than 18 or if parent_permission is true' do
+    person_object = Person.new 18, 'Person', parent_permission: false
+    expect(person_object.can_use_services?).to eq true
+    person_object = Person.new 12, 'Person', parent_permission: true
+    expect(person_object.can_use_services?).to eq true
+    person_object = Person.new 12, 'Person', parent_permission: false
+    expect(person_object.can_use_services?).to eq false
+  end
+
+  it 'should add a new rental' do
+    person_object = Person.new 12, 'Person', parent_permission: true
+    book=double("book")
+    allow(book).to receive (:rentals) {[]}
+    person_object.add_rental(book,"27-07-2022")
+    expect(person_object.rentals.length).to be 1 
+  end
 end
